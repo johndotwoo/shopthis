71e70eb7ccec9582ceeeeb6d28c90d42f18b015a
johndotwoo <johndotwoo@hotmail.com>
2014-05-13 23:41:41 -0400

    implemented checkbox to mark completion
    added checkbox for each list item

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('.button.tiny.left').click(function() {
 		var entryFromUser = $("#formEntryFromUser").val();
-		$('.small-block-grid-1.text-center').append('<li><span>' + entryFromUser + '</span><button class="button alert tiny right">Remove me!</button></li>');
+		$('.small-block-grid-1.text-center').append('<li><input type="checkbox"><span>' + ' ' + entryFromUser + '</span><button class="button alert tiny right">Remove me!</button></li>');
 	})
 
 	$('#shopping-list').on('click','.button.alert.tiny.right',function() {
