fc8bba12e8c998b748a61efdc1000d29abfcf75e
johndotwoo <johndotwoo@hotmail.com>
2014-05-13 23:17:00 -0400

    implemented "remove from list" feature
    "remove from list" feature is working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-	$('.button.tiny.left').click(function(){
+	$('.button.tiny.left').click(function() {
 		var entryFromUser = $("#formEntryFromUser").val();
-		$('.small-block-grid-1.text-center').append('<li>' + entryFromUser + '<button class="button alert tiny right">Remove me!</button></li>');
+		$('.small-block-grid-1.text-center').append('<li><span>' + entryFromUser + '</span><button class="button alert tiny right">Remove me!</button></li>');
+	})
+
+	$('#shopping-list').on('click','.button.alert.tiny.right',function() {
+		$(this).closest('li').remove();
 	});
 });
